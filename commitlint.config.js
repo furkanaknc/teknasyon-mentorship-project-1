38e49e85e2aa0b1457c68a3273573ff113d959ae
Furkan Akıncı <furkanaknc55@gmail.com>
2025-03-21 18:40:18 +0300

    build: start project

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "ci",
+        "build",
+        "revert",
+        "build",
+      ],
+    ],
+    "type-case": [2, "always", "lowercase"],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+  },
+};
