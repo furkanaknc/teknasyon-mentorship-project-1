40e97a383df4432d5b37a1e45d7c8866c1ccf8c2
Furkan Akıncı <furkanaknc55@gmail.com>
2025-06-02 01:15:02 +0300

    feat(profile): add logger to app

--- src/apps/profile/src/validations/common/environment.validation.ts
+++ src/apps/profile/src/validations/common/environment.validation.ts
@@ -4,6 +4,7 @@ export const envSchema = z.object({
   PROFILE_PORT: z.coerce.number().default(3003),
 
   DATABASE_URL: z.string(),
+  LOG_LEVEL: z.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']).default('info'),
 });
 
 export type EnvVariables = z.infer<typeof envSchema>;
